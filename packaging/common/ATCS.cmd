3b99a946540753adb5864ed1448bf0052269dc66
OMGeeky <>
2025-09-23 19:29:28 +0200

    add comments to start script about min required java version; update nsi script to match standalone start script

--- packaging/common/ATCS.cmd
+++ packaging/common/ATCS.cmd
@@ -2,6 +2,7 @@
 
 set "ATCS_DIR=%~dp0"
 set "MAX_MEM=1024M"
+REM required minimum java version is 11
 set "JAVA=java.exe"
 set "JAVA_OPTS=-DFONT_SCALE=1.0 -Dswing.aatext=true"
 set "ENV_FILE=%ATCS_DIR%ATCS.env.bat"
@@ -10,6 +11,7 @@ if exist "%ENV_FILE%" (
   call "%ENV_FILE%"
 ) else (
   echo REM set "MAX_MEM=%MAX_MEM%">"%ENV_FILE%"
+  echo REM required minimum java version is 11
   echo REM set "JAVA=%JAVA%">>"%ENV_FILE%"
   echo REM set "JAVA_OPTS=%JAVA_OPTS%">>"%ENV_FILE%"
   echo.>>"%ENV_FILE%"
